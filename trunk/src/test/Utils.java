12d0d34f19619483518b7399d8c1da5c0f524278
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-05-24 08:33:04 +0000

    First version of a simple testing environment (Java-based so far) to try out XML-RPC calls from a GUI.

---
+++ trunk/src/test/Utils.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * This class provides reusable static utilities.
+ * 
+ * @author Anton Huttenlocher
+ *
+ */
+public class Utils {
+
+    public static String getTimestamp() {
+        Locale currentLocale = new Locale("de", "DE");
+        DateFormat timestamp = 
+            DateFormat.getDateTimeInstance(DateFormat.SHORT,
+                                           DateFormat.DEFAULT,
+                                           currentLocale);
+        return timestamp.format(new Date());
+    }
+    
+}
