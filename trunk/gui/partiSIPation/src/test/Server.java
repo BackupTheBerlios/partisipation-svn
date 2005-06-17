fb08ce39595f0f929151d30da63f649e201136e3
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-06-17 23:39:21 +0000

    GUI host and port have been made private static constants.

--- trunk/gui/partiSIPation/src/test/Server.java
+++ trunk/gui/partiSIPation/src/test/Server.java
@@ -12,14 +12,17 @@ import org.apache.xmlrpc.XmlRpc;
  *
  */
 public class Server {
+    
+    private static String GUI_HOST = "192.168.0.3";
+    private static int GUI_PORT = 8888;
 
     public static void main(String[] args) {
         // turn XML-view of method calls on 
         XmlRpc.setDebug(true);
         try {
-            WebServer server = new WebServer(7777, InetAddress.getLocalHost());
+            WebServer server = new WebServer(GUI_PORT, InetAddress.getByName(GUI_HOST));
             server.start();
-            server.addHandler("sip", new RequestProcessor());
+            server.addHandler("gui", new RequestProcessor());
         } catch (UnknownHostException uhe) {
 
         }
