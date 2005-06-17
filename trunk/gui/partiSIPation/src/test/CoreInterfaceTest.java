4378a29e01a3639ce8ddd8ea683c7026b12c6732
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-06-17 23:44:18 +0000

    A JUnit test: GUI executes complete core functionality. First version.

---
+++ trunk/gui/partiSIPation/src/test/CoreInterfaceTest.java
@@ -0,0 +1,252 @@
+package test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.apache.xmlrpc.WebServer;
+import org.apache.xmlrpc.XmlRpcClientLite;
+import org.apache.xmlrpc.XmlRpcException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+
+/**
+ * JUnit test case for the core interface.
+ *
+ * @author Anton Huttenlocher
+ * @author Oleksiy Reznikov
+ */
+public class CoreInterfaceTest extends TestCase {
+    
+    XmlRpcClientLite client;
+    WebServer server;
+    Vector params = new Vector();
+    String GUI_HOST = "192.168.0.3";
+    String CORE_HOST = "192.168.0.3";
+    int GUI_PORT = 8888;
+    int CORE_PORT = 7777;
+    
+    public CoreInterfaceTest() {
+            try {
+                client = new XmlRpcClientLite(InetAddress.getByName(CORE_HOST).getHostName(), CORE_PORT);
+            /*    server = new WebServer(GUI_PORT, InetAddress.getByName("192.168.0.3"));
+                
+                server.start();
+                server.addHandler("gui", new RequestProcessor()); */
+                
+            } catch (MalformedURLException e) {
+                fail("Failed: create client.");
+            } catch (UnknownHostException e) {
+                fail("Failed: create client.");
+            }
+    }
+    
+    public void testRegisterGui() {
+        params.clear();
+        params.add(GUI_HOST);
+        params.add(new Integer(GUI_PORT));
+        String expected = "OK";
+        try {
+            Assert.assertTrue(expected.equals((String) client.execute("core.registerGui", params)));
+        } catch (XmlRpcException e) {
+            fail("Failed: registerGui");
+        } catch (IOException e) {
+            fail("Failed: registerGui");
+        }
+    }
+    
+    public void testRegister() {
+        params.clear();
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.register", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: register");
+        } catch (IOException e) {
+            fail("Failed: register");
+        }
+    }
+    
+    public void testUnregister() {
+        params.clear();
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.register", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: register");
+        } catch (IOException e) {
+            fail("Failed: register");
+        }
+    }
+    
+    public void testMakeCall() {
+        params.clear();
+        params.addElement(new Integer(0));
+        params.addElement("MUSTERMANN");
+        try {
+            Assert.assertTrue(client.execute("core.makeCall", params) instanceof Integer);
+        } catch (XmlRpcException e) {
+            fail("Failed: makeCall");
+        } catch (IOException e) {
+            fail("Failed: makeCall");
+        }
+    }
+    
+    public void testEndCall() {
+        params.clear();
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.endCall", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testAcceptCall() {
+        params.clear();
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.acceptCall", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testSetSpeakerVolume() {
+        params.clear();
+        params.add(new Double(0.5));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.setSpeakerVolume", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testGetSpeakerVolume() {
+        params.clear();
+        try {
+            Assert.assertTrue(client.execute("core.getSpeakerVolume", params) instanceof Double);
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testSetMicroVolume() {
+        params.clear();
+        params.add(new Double(0.5));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.setMicroVolume", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testGetMicroVolume() {
+        params.clear();
+        try {
+            Assert.assertTrue(client.execute("core.getMicroVolume", params) instanceof Double);
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testSendDtmf() {
+        params.clear();
+        params.add(new String("#"));
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.sendDtmf", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testAccountGetAll() {
+        params.clear();
+        try {
+            Object o = client.execute("core.accountGetAll", params);
+            Assert.assertTrue(o instanceof Vector);
+            Vector v = (Vector) o;
+            Enumeration e = v.elements();
+            while (e.hasMoreElements()) {
+                Object n = e.nextElement();
+                Assert.assertTrue(n instanceof Integer);
+            }
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+            e.printStackTrace();
+        } catch (IOException e) {
+            fail("Failed: endCall");
+            e.printStackTrace();
+        }
+    }
+    
+    public void testAccountSet() {
+        params.clear();
+        params.addElement(new Integer(0));
+        params.addElement("ATTRIBUTE");
+        params.addElement("VALUE");
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.accountSet", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: makeCall");
+        } catch (IOException e) {
+            fail("Failed: makeCall");
+        }
+    }
+    
+    public void testAccountGet() {
+        params.clear();
+        params.addElement(new Integer(0));
+        params.addElement("MUSTERMANN");
+        try {
+            Assert.assertTrue(client.execute("core.accountGet", params) instanceof String);
+        } catch (XmlRpcException e) {
+            fail("Failed: makeCall");
+        } catch (IOException e) {
+            fail("Failed: makeCall");
+        }
+    }
+    
+    public void testAccountCreate() {
+        params.clear();
+        try {
+            Assert.assertTrue(client.execute("core.accountCreate", params) instanceof Integer);
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+    
+    public void testAccountDelete() {
+        params.clear();
+        params.add(new Integer(0));
+        try {
+            Assert.assertTrue(((Boolean) client.execute("core.accountDelete", params)).booleanValue());
+        } catch (XmlRpcException e) {
+            fail("Failed: endCall");
+        } catch (IOException e) {
+            fail("Failed: endCall");
+        }
+    }
+}
