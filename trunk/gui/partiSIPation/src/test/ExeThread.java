825941ccb314a560ca31b4195c435d7eca690003
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-07-14 17:09:37 +0000

    Bug removed: thread waited the full pause period even if the XML-RPC call had successfully returned earlier.

--- trunk/gui/partiSIPation/src/test/ExeThread.java
+++ trunk/gui/partiSIPation/src/test/ExeThread.java
@@ -1,12 +1,25 @@
 package test;
 
+/**
+ * This class prevents the GUI from hanging if a XML-RPC call cannot return due
+ * to absence of the core or other reasons by managing a separate thread for 
+ * each call, which is interrupted either after a timeout or immediately after 
+ * a successful return of the RPC call.
+ * 
+ * @author Anton Huttenlocher
+ * @author Oleksiy Reznikov
+ *  
+ */
+
 import java.io.IOException;
 import java.util.Vector;
+
+import javax.swing.JOptionPane;
 
 import org.apache.xmlrpc.XmlRpcClientLite;
 import org.apache.xmlrpc.XmlRpcException;
 
-class ExeThread extends Thread {
+class ExeThread {
 
     private String func;
 
@@ -14,10 +27,17 @@ class ExeThread extends Thread {
 
     private Object result = null;
     
-    private int pause = 5000;
+    private int pause = 3000;
     
     private XmlRpcClientLite client = null;
-
+    
+    private String XMLRPC_CALL_TIMEOUT = "XML-PRC CALL TIMEOUT. \n"
+        + "CORE UNREACHABLE.";
+    
+    private Thread watchthread = null;
+    
+    private boolean finished = false;
+    
     public ExeThread(XmlRpcClientLite c, String f, Vector p) {
         client = c;
         func = f;
@@ -25,30 +45,50 @@ class ExeThread extends Thread {
     }
 
     public Object execute() {
-
-        this.start();
-        try {
-            Thread.sleep(pause);
-        } catch (InterruptedException e) {
-            return result;
+        
+        watchthread = new WatchThread();
+        watchthread.start();
+        Thread exe = new ExecuteThread();
+        exe.start();
+      
+        while (!finished) {
+            
         }
+        
+        if (result == null) {
+            JOptionPane.showMessageDialog(null, XMLRPC_CALL_TIMEOUT, "ERROR",
+                    JOptionPane.ERROR_MESSAGE);
+        } 
         return result;
     }
 
-
-    public void run() {
-        try {
-            result = client.execute(func, par);
-            this.interrupt();
-        } catch (XmlRpcException e1) {
-            result = null;
-        } catch (IOException e1) {
-            result = null;
-        }
-    }
-    
     public void setTimeout(int timeout) {
         pause = timeout;
     }
+    
+    class ExecuteThread extends Thread {
+        public void run() {
+            try {
+                result = client.execute(func, par);
+                watchthread.interrupt();
+            } catch (XmlRpcException e1) {
+                	result = null;
+                	finished = true;
+            } catch (IOException e1) {
+                	result = null;
+                	finished = true;
+            }
+        }
+    }
+    
+    class WatchThread extends Thread {
+        public void run() {
+            try {
+                sleep(pause);
+            } catch (InterruptedException e) {
+                finished = true;
+            }
+            finished = true;
+        }
+    }
 }
-
