4905e87a242308196f45046ff42b3440f441a02b
enrico <enrico@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-07-27 20:29:53 +0000

    added test case: register an user

--- trunk/core/sipstack/test_osip.c
+++ trunk/core/sipstack/test_osip.c
@@ -1,14 +1,23 @@
-#include "check.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <check.h>
 #include "sip_stack_interface.h"
 
-START_TEST (test_osip_ifapt)
+START_TEST (test_sipstack_ifapt)
 {
     /* unit test code */
-    //int isRegistered = register(...);
+
+    int i = sipstack_init();
+
+    int regId = sipstack_send_register("sip:321@192.168.0.2", "sip:192.168.0.2", 1800);
+    fail_unless(regId > -1, "Sending REGISTER failed. (result = %2d)", regId);
+    i = sipstack_update_register(regId, 1800);
+    fail_unless(i == 0, "Updating REGISTER failed. (result = %2d)", i);
+    i = sipstack_unregister(regId);
+    fail_unless(i == 0, "Unregistering failed. (result = %2d)", i);
 
     /* start call and quit it*/
     //int callID = start_call();
-    //fail_unless(quit_call(callID));
 
     /* start call and cancel it*/
     //callID = start_call();
@@ -22,3 +31,28 @@ START_TEST (test_osip_ifapt)
 
 }
 END_TEST
+
+Suite *sipstack_suite (void)
+{
+  Suite *s = suite_create("sipstack");
+  TCase *tc_apt = tcase_create("Adapter");
+
+  suite_add_tcase (s, tc_apt);
+
+  tcase_add_test (tc_apt, test_sipstack_ifapt);
+
+  //tcase_add_checked_fixture (tc_apt, setup, teardown);
+
+  return s;
+}
+
+int main (void)
+{
+  int nf;
+  Suite *s = sipstack_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all (sr, CK_NORMAL);
+  nf = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
