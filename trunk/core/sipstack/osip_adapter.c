a6510007b10911443287e298cc818bcce03bf576
enrico <enrico@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-10 21:51:09 +0000

    finalized sipstack_register(), sipstack_update_register(), sipstack_unregister()

--- trunk/core/sipstack/osip_adapter.c
+++ trunk/core/sipstack/osip_adapter.c
@@ -28,6 +28,10 @@ sipstack_init() {
     }
 }
 
+int
+sipstack_quit() {
+    eXosip_quit();
+}
 /**
     send initial register
     @param char* identity
@@ -39,6 +43,9 @@ sipstack_send_register(char *const identity, char *const registrar, int expire) 
     osip_message_t *reg = NULL;
     int id;
     int i;
+
+    /*DEBUG*/
+    /*fprintf(stdout, "\n[initial registration] register user...\n");*/
 
     eXosip_lock();
     id = eXosip_register_build_initial_register(identity, registrar, NULL,
@@ -54,6 +61,33 @@ sipstack_send_register(char *const identity, char *const registrar, int expire) 
 
     i = eXosip_register_send_register(id, reg);
     eXosip_unlock();
+
+    if (i < 0) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[initial registration] sending REGISTER failed");*/
+        return -1;
+    }
+
+    eXosip_event_t *je;
+
+    je = eXosip_event_wait(0, 50);
+    eXosip_automatic_action();
+
+    if(je==NULL) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[initial registration] no response received");*/
+        return -1;
+    }else {
+        /*DEBUG*/
+        /*fprintf(stdout, "[initial registration] Event '%s' received..\n", je->textinfo);*/
+        /*fprintf(stdout, "[initial registration] response = '%i'\n", je->response->status_code);*/
+    }
+
+    /*DEBUG*/
+    /*fprintf(stdout, "[initial registration] ...done\n");*/
+
     return id;
 }
 
@@ -62,15 +96,49 @@ sipstack_unregister(int id) {
     osip_message_t *reg = NULL;
     int i;
 
+    /*DEBUG*/
+    /*fprintf(stdout, "\n[unregistration] unregister user...\n");*/
+
     eXosip_lock();
     i = eXosip_register_build_register(id, 0, &reg);
 
     if (i < 0) {
         eXosip_unlock();
+        /*DEBUG*/
+        /*fprintf(stderr, "[unregistration] cannot build REGISTER\n");*/
         return -1;
     }
+    /*DEBUG*/
+    /*fprintf(stdout, "[unregistration] REGISTER build\n");*/
+
     i = eXosip_register_send_register(id, reg);
     eXosip_unlock();
+    if (i < 0) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[unregistration] sending REGISTER failed");*/
+        return -1;
+    }
+
+    eXosip_event_t *je;
+
+    je = eXosip_event_wait(0, 50);
+    eXosip_automatic_action();
+
+    if(je==NULL) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[unregistration] no response received");*/
+        return -1;
+    }else {
+        /*DEBUG*/
+        /*fprintf(stdout, "[unregistration] Event '%s' received..\n", je->textinfo);*/
+        /*fprintf(stdout, "[unregistration] response = '%i'\n", je->response->status_code);*/
+    }
+
+    /*DEBUG*/
+    /*fprintf(stdout, "[unregistration] ...done\n");*/
+
     return i;
 }
 
@@ -79,16 +147,49 @@ sipstack_update_register(int id, int expire) {
     osip_message_t *reg = NULL;
     int i;
 
+    /*DEBUG*/
+    /*fprintf(stdout, "\n[update registration] update registration of user...\n");*/
+
     eXosip_lock();
     i = eXosip_register_build_register(id, expire, &reg);
 
     if (i < 0) {
         eXosip_unlock();
-        fprintf(stdout, "cannot build REGISTER\n");
+        /*DEBUG*/
+        /*fprintf(stderr, "[update registration] cannot build REGISTER\n");*/
         return -1;
     }
+    /*DEBUG*/
+    /*fprintf(stdout, "[update registration] REGISTER build\n");*/
+
     i = eXosip_register_send_register(id, reg);
     eXosip_unlock();
+    if (i < 0) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[update registration] sending REGISTER failed");*/
+        return -1;
+    }
+
+    eXosip_event_t *je;
+
+    je = eXosip_event_wait(0, 50);
+    eXosip_automatic_action();
+
+    if(je==NULL) {
+        eXosip_quit();
+        /*DEBUG*/
+        /*fprintf(stdout, "[update registration] no response received");*/
+        return -1;
+    }else {
+        /*DEBUG*/
+        /*fprintf(stdout, "[update registration] Event '%s' received..\n", je->textinfo);*/
+        /*fprintf(stdout, "[update registration] response = '%i'\n", je->response->status_code);*/
+    }
+
+    /*DEBUG*/
+    /*fprintf(stdout, "[update registration] ...done\n");*/
+
     return i;
 }
 
