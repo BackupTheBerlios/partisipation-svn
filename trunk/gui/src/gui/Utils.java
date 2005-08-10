45946eadc8293c423158bd058972608e9f2a501d
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-10 21:10:04 +0000

    Centralized storage for port and host data.

--- trunk/gui/src/gui/Utils.java
+++ trunk/gui/src/gui/Utils.java
@@ -6,19 +6,27 @@ import java.util.Locale;
 
 /**
  * This class provides reusable static utilities.
- * 
+ *
  * @author Anton Huttenlocher
  *
  */
 public class Utils {
 
+    public final static String GUIHOST = "192.168.0.1";
+
+    public final static String COREHOST = "192.168.0.1";
+
+    public final static int GUIPORT = 8888;
+
+    public final static int COREPORT = 7777;
+
     public static String getTimestamp() {
         Locale currentLocale = new Locale("de", "DE");
-        DateFormat timestamp = 
+        DateFormat timestamp =
             DateFormat.getDateTimeInstance(DateFormat.SHORT,
                                            DateFormat.DEFAULT,
                                            currentLocale);
         return timestamp.format(new Date());
     }
-    
+
 }
