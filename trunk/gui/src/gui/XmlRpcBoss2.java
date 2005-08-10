e9fece86f2e747e2cac7332be026f4e94ee87ebe
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-10 21:13:01 +0000

    Some style fixes.

--- trunk/gui/src/gui/XmlRpcBoss2.java
+++ trunk/gui/src/gui/XmlRpcBoss2.java
@@ -1,96 +1,100 @@
 package gui;
 
 import java.net.UnknownHostException;
+import org.apache.xmlrpc.WebServer;
 import org.apache.xmlrpc.XmlRpcClientLite;
+import org.apache.xmlrpc.XmlRpcException;
+
 import java.util.Vector;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import org.apache.xmlrpc.XmlRpcException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
- * <p>XmlRpcBoss2.java</p>
+ * This class allows to run GUI test scenarios by simulating core activity. The
+ * control is done by sending arbitrarily parameterized XML-RPC calls from the
+ * console thus shell scripts can be used to do the job.
+ * 
  * @author Anton Huttenlocher
  * @author Oleksiy Reznikov
  */
 public class XmlRpcBoss2 {
 
-  static String GUI_HOST = "192.168.0.1";
+    private static String HOST = "192.168.0.1";
 
-  static int GUI_PORT = 8888;
+    private static int GUI_PORT = 8888;
 
-  public static void main(String[] args) throws IOException, XmlRpcException {
-    try {
+    private static int CORE_PORT = 7777;
+    
+    public static void main(String[] args) {
+        
+        Vector params = new Vector();
 
-      Vector params = new Vector();
-      params.clear();
-      String method;
+        try {
+            
+            XmlRpcClientLite client = new XmlRpcClientLite(InetAddress.getByName(
+                    HOST).getHostName(), GUI_PORT);
 
-      if (args[0].equalsIgnoreCase("changeRegStatus")) {
-        method = "gui.changeRegStatus";
-        params.addElement(new Integer(args[1]));
-        params.addElement(new Boolean(args[2]));
-      }
-      else if (args[0].equalsIgnoreCase("changeCallStatus")) {
-        method = "gui.changeCallStatus";
-        params.addElement(new Integer(args[1]));
-        params.addElement(new String(args[2]));
-      }
-      else if (args[0].equalsIgnoreCase("showUserEvent")) {
-        method = "gui.showUserEvent";
-        params.addElement(new Integer(args[1]));
-        params.addElement(new String(args[2]));
-        params.addElement(new String(args[3]));
-        params.addElement(new String(args[4]));
-        params.addElement(new String(args[5]));
-      }
-      else if (args[0].equalsIgnoreCase("registerCore")) {
-        method = "gui.registerCore";
-      }
+                params.clear();
+                String method;
 
-      else if (args[0].equalsIgnoreCase("incomingCall")) {
-        method = "gui.incomingCall";
-        params.addElement(new Integer(args[1]));
-        params.addElement(new Integer(args[2]));
-        params.addElement(new String(args[3]));
-        params.addElement(new String(args[4]));
-      }
+                 if (args[0].equalsIgnoreCase("changeRegStatus")) {
+                     method = "gui.changeRegStatus";
+                     params.addElement(new Integer(args[1]));
+                     params.addElement(new Boolean(args[2]));
+                 } else if (args[0].equalsIgnoreCase("changeCallStatus")) {
+                     method = "gui.changeCallStatus";
+                     params.addElement(new Integer(args[1]));
+                     params.addElement(new String(args[2]));
+                 } else if (args[0].equalsIgnoreCase("showUserEvent")) {
+                     method = "gui.showUserEvent";
+                     params.addElement(new Integer(args[1]));
+                     params.addElement(new String(args[2]));
+                     params.addElement(new String(args[3]));
+                     params.addElement(new String(args[4]));
+                     params.addElement(new String(args[5]));
+                 } else if (args[0].equalsIgnoreCase("registerCore")) {
+                     method = "gui.registerCore";
+                 }
+                 else if (args[0].equalsIgnoreCase("incomingCall")) {
+                    method = "gui.incomingCall";
+                    params.addElement(new Integer(args[1]));
+                    params.addElement(new Integer(args[2]));
+                    params.addElement(new String(args[3]));
+                    params.addElement(new String(args[4]));
+                }
 
-      else if (args[0].equalsIgnoreCase("setSpeakerVolume")) {
-        method = "gui.setSpeakerVolume";
-        params.addElement(new Double(args[1]));
-      }
-      else if (args[0].equalsIgnoreCase("setMicroVolume")) {
-        method = "gui.setMicroVolume";
-        params.addElement(new Double(args[1]));
+                else if (args[0].equalsIgnoreCase("setSpeakerVolume")) {
+                    method = "gui.setSpeakerVolume";
+                    params.addElement(new Double(args[1]));
+                } else if (args[0].equalsIgnoreCase("setMicroVolume")) {
+                    method = "gui.setMicroVolume";
+                    params.addElement(new Double(args[1]));
 
-      }
-      else {
-        System.out.println("No provides no such method");
-        return;
-      }
+                } else {
+                    System.out.println("No such method");
+                    return;
+                }
 
-      XmlRpcClientLite client = new XmlRpcClientLite(InetAddress.getByName(
-          GUI_HOST).getHostName(), GUI_PORT);
+               
+                ExeThread t = new ExeThread(client, method, params);
+                t.setTimeout(100000000);
+                t.execute();
 
-      ExeThread t = new ExeThread(client, method, params);
-      t.setTimeout(300);
-      t.execute();
-
+            //}
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
     }
-    catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    catch (UnknownHostException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-
-    }
-
-  }
 }
