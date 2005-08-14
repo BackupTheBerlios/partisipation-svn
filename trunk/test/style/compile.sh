89781d94038b1948d4187baf84431d606a5943f5
matthias <matthias@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-14 22:18:39 +0000

    - changed style test to current settings of the project

--- trunk/test/style/compile.sh
+++ trunk/test/style/compile.sh
@@ -1,13 +1,21 @@
 indent example_input.c -o example_std.c
 indent -kr example_input.c -o example_kr.c
 indent -orig example_input.c -o example_orig.c
-indent -l80 -nut -i4 \
-  -bad -bap \
-  -fca -fc1 -c33 -cd33 -cp33 -lc80 \
-  -bbb -nsob -d0 -cdb -nsc \
-  -br -ce -cdw -cli4 -cbi0 \
-  -nss -npcs -cs -nbs \
-  -saf -sai -saw -nprs \
-  -bc -nbfda -nbfde -psl -brf \
-  -lp -ip4 -ppi4 -nlps -bbo \
-  example_input.c -o example_sipoks.c
+indent --line-length80 --no-tabs --indent-level4 \
+--blank-lines-after-declarations --blank-lines-after-procedures \
+--blank-lines-after-procedure-declarations --dont-format-comments \
+--dont-format-first-column-comments --comment-indentation33 \
+--declaration-comment-column33 --else-endif-column33 --comment-line-length78 \
+--blank-lines-before-block-comments --leave-optional-blank-lines \
+--line-comments-indentation0 --comment-delimiters-on-blank-lines \
+--start-left-side-of-comments --braces-on-if-line --cuddle-else \
+--cuddle-do-while --case-indentation4 --case-brace-indentation0 \
+--dont-space-special-semicolon --no-space-after-function-call-names \
+--space-after-cast --no-blank-before-sizeof --space-after-for \
+--space-after-if --space-after-while --no-space-after-parentheses \
+--blank-lines-after-commas --dont-break-function-decl-args \
+--dont-break-function-decl-args-end --procnames-start-lines \
+--braces-on-func-def-line --continue-at-parentheses --parameter-indentation4 \
+--preprocessor-indentation4 --remove-preprocessor-space \
+--break-before-boolean-operator --honour-newlines \
+example_input.c -o example_sipoks.c
