bd545f3f341a462543915922acf6a0c587b9c7da
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-17 20:49:14 +0000

    - header file for the double-linked list

---
+++ trunk/core/xmlrpc-test/accounts/list.h
@@ -0,0 +1,33 @@
+#ifndef LIST_H
+#    define LIST_H
+
+typedef struct account {
+    int id;
+    char *name;
+    char *username;
+    char *domain;
+    char *authusername;
+    char *password;
+    char *displayname;
+    char *outboundproxy;
+    char *registrar;
+    int autoregister;
+} typeaccount;
+
+typedef struct node {
+    struct account *acc;
+    struct node *next;
+    struct node *prev;
+} typenode;
+
+void add_node(struct account *);
+
+void del_node(int);
+
+struct node *get_node(int);
+
+extern struct node *head;
+
+extern struct node *tail;
+
+#endif
