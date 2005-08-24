35604b92f73d1cab144a413c06ef0c369351ce59
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-24 21:57:00 +0000

    - new function set_xml_source (to be able to run Check) - parameter of account_get() changed, return of char* does not work

--- trunk/core/xmlrpc-test/accounts/account_management.h
+++ trunk/core/xmlrpc-test/accounts/account_management.h
@@ -1,9 +1,13 @@
+void set_xml_source(char *file);
+
+void account_management_init();
+
 void account_get_all(int *accountIds, int *length);
 
 int account_set(int const accountId, char *const attribute,
 				char *const value);
 
-char *account_get(int const accountId, char *const attribute);
+void account_get(int const accountId, char *const attribute, char *result);
 
 int account_create();
 
