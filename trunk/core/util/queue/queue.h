2d876a0062337582f0d3589928fdb453bb4ef26d
enrico <enrico@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-24 17:23:50 +0000

    created doxygen module 'utilities' and added it as a submodule 'queue'

--- trunk/core/util/queue/queue.h
+++ trunk/core/util/queue/queue.h
@@ -1,38 +1,71 @@
+/**
+ * @file queue.h
+ * Queue
+ *
+ * Define queue record structure and all forward declarations.
+ *
+ * @author Jeffrey Hunter (http://www.iDevelopment.info)
+ * @author Enrico Hartung <enrico@iptel.org>
+ */
 
-/*
- *  FILE   : queue.h
- *  AUTHOR : Jeffrey Hunter
- *  WEB    : http://www.iDevelopment.info
- *  NOTES  : Define queue record structure and
- *           all forward declarations.
- */ 
-	
+/**
+ * @defgroup utils Utilities
+ * Utility collection
+ * @}
+ */
+
+/**
+ * @defgroup queue Queue
+ * @ingroup utils
+ * @{
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-	
+
 #ifndef QUEUE_DATATYPE
 #include "queue_datatype_int.h"
-#endif	/*  */
+#endif	/*
+ */
 #ifdef QUEUE_DATATYPE
 #include QUEUE_DATATYPE_H
-#endif	/*  */
-	
+#endif	/*
+ */
+
 #define Error(Str)        FatalError(Str)
 #define FatalError(Str)   fprintf(stderr, "%s\n", Str), exit(1)
-	
+
 #ifndef _Queue_h
 #define _Queue_h
-	struct QueueRecord;
-typedef struct QueueRecord *Queue;
-int IsEmpty(Queue Q);
-int IsFull(Queue Q);
-Queue CreateQueue(int MaxElements);
-void DisposeQueue(Queue Q);
-void MakeEmpty(Queue Q);
-void Enqueue(ElementType X, Queue Q);
-ElementType Front(Queue Q);
-void Dequeue(Queue Q);
-ElementType FrontAndDequeue(Queue Q);
-int IsElement(ElementType X, Queue Q);
-
+
+struct QueueRecord;
+
+typedef struct QueueRecord *Queue;
+
+
+int IsEmpty(Queue Q);
+
+int IsFull(Queue Q);
+
+Queue CreateQueue(int MaxElements);
+
+void DisposeQueue(Queue Q);
+
+void MakeEmpty(Queue Q);
+
+void Enqueue(ElementType X, Queue Q);
+
+ElementType Front(Queue Q);
+
+void Dequeue(Queue Q);
+
+ElementType FrontAndDequeue(Queue Q);
+
+int IsElement(ElementType X, Queue Q);
+
+
 #endif	/* _Queue_h */
+
+/**
+ * @}
+ */
