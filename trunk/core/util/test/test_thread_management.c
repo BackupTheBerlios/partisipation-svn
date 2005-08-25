b5740c9bc311ce27b6d5824bada78d33b86447ce
matthias <matthias@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-25 02:03:03 +0000

    - test of thread management now uses check framework

--- trunk/core/util/test/test_thread_management.c
+++ trunk/core/util/test/test_thread_management.c
@@ -2,46 +2,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <check.h>
 
 #include <thread_management.h>
 
-void *test_thread(void *args) {
+void setup(void) {
+	int rc;
+	rc = tm_init();
+	fail_if(rc == 0, "thread management could not be initialized");
+}
 
-	printf("test_thread entered\n");
-
-	sleep(2);
-	printf("leaving test_thread\n");
-
-	thread_terminated(pthread_self());
+void teardown(void) {
+	int rc;
+	rc = tm_destroy();
+	fail_if(rc == 0, "thread management could not be released");
 	pthread_exit(NULL);
 }
 
-void *test_thread2(void *args) {
+void *thrd_start_thread(void *args) {
+	int rc;
+	sleep(2);
+	rc = thread_terminated(pthread_self());
+	fail_if(rc == 0, "thread referenced could not be removed");
+	pthread_exit(NULL);
+}
 
+void *thrd_simple_arguments(void *args) {
 	int i;
-
-	printf("test_thread2 entered\n");
+	int rc;
 
 	i = (int) args;
-	printf("i is: %d\n", i);
-	sleep(5);
-	printf("leaving test_thread2\n");
+	fail_unless(i == 2842, "argument has not expected value");
+	sleep(2);
 
-	thread_terminated(pthread_self());
+	rc = thread_terminated(pthread_self());
+	fail_if(rc == 0, "thread referenced could not be removed");
 	pthread_exit(NULL);
 }
 
-int main() {
-	int i = 2842;
-	int x;
+void *thrd_simple_return_value(void *args) {
+	int rc;
 
-	tm_init();
-	start_thread(test_thread, NULL);
-	start_thread(test_thread2, (void *) i);
-	scanf("%d", &x);
-	start_thread(test_thread, NULL);
-	scanf("%d", &x);
-	tm_destroy();
-	pthread_exit(NULL);
+	sleep(2);
 
+	rc = thread_terminated(pthread_self());
+	fail_if(rc == 0, "thread referenced could not be removed");
+	pthread_exit((void *) 4711);
+}
+
+// *INDENT-OFF*
+
+START_TEST(test_start_thread) {
+	int rc;
+	rc = start_thread(thrd_start_thread, NULL);
+	fail_if(rc == 0, "thread could not be started");
+	sleep(6);
+} END_TEST 
+
+START_TEST(test_start_multiple_threads) {
+	int rc;
+	rc = start_thread(thrd_start_thread, NULL);
+	fail_if(rc == 0, "thread could not be started");
+	rc = start_thread(thrd_start_thread, NULL);
+	fail_if(rc == 0, "thread could not be started");
+	rc = start_thread(thrd_start_thread, NULL);
+	fail_if(rc == 0, "thread could not be started");
+	rc = start_thread(thrd_start_thread, NULL);
+	fail_if(rc == 0, "thread could not be started");
+	sleep(10);
+} END_TEST 
+
+START_TEST(test_simple_arguments) {
+	int i;
+	int rc;
+	i = 2842;
+	rc = start_thread(thrd_simple_arguments, (void *) i);
+	fail_if(rc == 0, "thread could not be started");
+	sleep(6);
+} END_TEST 
+
+START_TEST(test_simple_return_value) {
+	int rc;
+	rc = start_thread(thrd_simple_return_value, NULL);
+	fail_if(rc == 0, "thread could not be started");
+
+} END_TEST 
+
+// *INDENT-ON*
+
+Suite *tm_suite(void) {
+	Suite *s = suite_create("thread management\n\n");
+	TCase *tc_start = tcase_create("Start Thread");
+	TCase *tc_start_multiple = tcase_create("Start Multiple Threads");
+	TCase *tc_simple_args = tcase_create("Simple Argument Passing");
+
+	suite_add_tcase(s, tc_start);
+	suite_add_tcase(s, tc_start_multiple);
+	suite_add_tcase(s, tc_simple_args);
+
+	tcase_add_test(tc_start, test_start_thread);
+	tcase_set_timeout(tc_start, 15);
+	tcase_add_checked_fixture(tc_start, setup, teardown);
+
+	tcase_add_test(tc_start_multiple, test_start_multiple_threads);
+	tcase_set_timeout(tc_start_multiple, 15);
+	tcase_add_checked_fixture(tc_start_multiple, setup, teardown);
+
+	tcase_add_test(tc_simple_args, test_simple_arguments);
+	tcase_set_timeout(tc_simple_args, 15);
+	tcase_add_checked_fixture(tc_simple_args, setup, teardown);
+
+	return s;
+}
+
+int main(void) {
+	int nf;
+	Suite *s = tm_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	nf = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	if (nf != 0) {
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
