d2e0ff62cd2b101455f25543cffe682abf17737b
matthias <matthias@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-08-25 07:17:14 +0000

    - test of call id generator now uses check framework

--- trunk/core/core/test/test_cig.c
+++ trunk/core/core/test/test_cig.c
@@ -2,62 +2,145 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <check.h>
 
 #include <../util/thread_management.h>
 #include <call_id_generator.h>
 #include <time.h>
 
-void test_plain() {
-	int id, i;
+#define MAX_IDS_PLAIN (10)
+#define MAX_IDS_THREADED (10)
 
-	for (i = 0; i < 10; i++) {
-		id = cig_generate_call_id();
-		printf("id: %d\n", id);
-	}
+int testIds[MAX_IDS_THREADED];
+int testIdsSize;
+pthread_mutex_t idsLock;
+
+void setup(void) {
+	int rc;
+	
+	rc = cig_init();
+	fail_if(rc == 0, "call id generator could not be initialized");
 }
 
-void *request_id(void *args) {
+void teardown(void) {
+	int rc;
+	
+	rc = cig_destroy();
+	fail_if(rc == 0, "call id generator could not be released");
+}
+
+void setup_with_tm(void) {
+	int rc;
+	
+	rc = tm_init();
+	fail_if(rc == 0, "thread management could not be initialized");
+	
+	rc = pthread_mutex_init(&idsLock, NULL);
+	fail_unless(rc == 0, "mutex could not be initialized");
+	
+	setup();
+}
+
+void teardown_with_tm(void) {
+	int rc;
+	
+	teardown();
+	
+	rc = pthread_mutex_destroy(&idsLock);
+	fail_unless(rc == 0, "mutex could not be released");
+	
+	rc = tm_destroy();
+	fail_if(rc == 0, "thread management could not be released");
+	pthread_exit(NULL);
+}
+
+void *thrd_request_id(void *args) {
 	int id;
 
 	/*
-	 * sleep up to 10 seconds 
+	 * sleep up to 15 seconds 
 	 */
 	sleep((int) args);
-
+	
 	id = cig_generate_call_id();
-	printf("id: %d\n", id);
-
+	
+	pthread_mutex_lock(&idsLock);
+	testIds[testIdsSize] = id;
+	testIdsSize++;
+	pthread_mutex_unlock(&idsLock);
+	
 	thread_terminated(pthread_self());
 	pthread_exit(NULL);
 }
 
-void test_threaded() {
-	int i, r;
 
+START_TEST(test_plain) {
+	int i, j;
+	int ids[MAX_IDS_PLAIN];
+
+	for (i = 0; i < MAX_IDS_PLAIN; i++) {
+		ids[i] = cig_generate_call_id();
+	}
+	
+	for (i = 0; i < MAX_IDS_PLAIN - 1; i++) {
+		for (j = i + 1; j < MAX_IDS_PLAIN; j++) {
+			fail_if(ids[i] == ids[j], "Identical IDs found - should never occur");
+		}
+	}
+} END_TEST
+
+START_TEST(test_threaded) {
+	int i, j, r;
+	
+	testIdsSize = 0;
+	
 	/*
 	 * initialize random generator 
 	 */
 	srand(time(NULL));
 
-	for (i = 0; i < 10; i++) {
-		r = rand() % 20;
-		start_thread(request_id, (void *) r);
+	for (i = 0; i < MAX_IDS_THREADED; i++) {
+		r = rand() % 15;
+		start_thread(thrd_request_id, (void *) r);
 	}
+	sleep(20);
+	
+	for (i = 0; i < MAX_IDS_THREADED - 1; i++) {
+		for (j = i + 1; j < MAX_IDS_THREADED; j++) {
+			fail_if(testIds[i] == testIds[j], "Identical IDs found - should never occur");
+		}
+	}
+	
+} END_TEST
+
+Suite *tm_suite(void) {
+	Suite *s = suite_create("call id generation\n\n");
+	TCase *tc_plain = tcase_create("Test plain generation");
+	TCase *tc_threaded = tcase_create("Test threaded generation");
+
+	suite_add_tcase(s, tc_plain);
+	suite_add_tcase(s, tc_threaded);
+
+	tcase_add_test(tc_plain, test_plain);
+	tcase_add_checked_fixture(tc_plain, setup, teardown);
+
+	tcase_add_test(tc_threaded, test_threaded);
+	tcase_set_timeout(tc_threaded, 30);
+	tcase_add_checked_fixture(tc_threaded, setup_with_tm, teardown_with_tm);
+
+
+	return s;
 }
 
-int main() {
-	int x;
-
-	tm_init();
-	cig_init();
-
-	test_plain();
-	test_threaded();
-
-	scanf("%d", &x);
-
-	cig_destroy();
-	tm_destroy();
-	pthread_exit(NULL);
-	return 0;
+int main(void) {
+	int nf;
+	Suite *s = tm_suite();
+	SRunner *sr = srunner_create(s);
+	srunner_run_all(sr, CK_NORMAL);
+	nf = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	if (nf != 0) {
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
