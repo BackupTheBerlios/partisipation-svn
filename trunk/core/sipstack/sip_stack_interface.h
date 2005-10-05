71bfdfdab1775f340591d14f65429087348fc627
enrico <enrico@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-10-05 21:14:05 +0000

    adapted parameters because call management has been removed from sip stack adapter

--- trunk/core/sipstack/sip_stack_interface.h
+++ trunk/core/sipstack/sip_stack_interface.h
@@ -85,42 +85,41 @@ int sipstack_send_register(char *const identity, char *const registrar, int expi
  * After sending the REGISTER this method waits for a response.
  * If no response is received in 5 seconds or an error is received this methode returns -1.
  *
- * @param id registration id returned by sipstack_send_register()
+ * @param regId registration id returned by sipstack_send_register()
  * @param expire new expiration time
  * @return result code
  */
-int sipstack_send_update_register(int id, int expire);
+int sipstack_send_update_register(int regId, int expire);
 
 /**
  * Deletes a registration. Therefore the expiration time is set to 0.
  * After sending the REGISTER this method waits for a response.
  * If no response is received in 5 seconds or an error is received this methode returns -1.
  *
- * @param id registration id returned by sipstack_send_register()
+ * @param regId registration id returned by sipstack_send_register()
  * @return result code
  */
-int sipstack_send_unregister(int id);
+int sipstack_send_unregister(int regId);
 
 /**
  * Send an initial INVITE. It returns a dialog id which is supposed to be used for
  * further messages of this call.
  *
- * @param callId call id for this call
  * @param to sip url of callee
  * @param from sip url of caller
  * @param subject subject of call
- * @return return code (-1 if sending of INVITE failed)
+ * @return call id (-1 if sending of INVITE failed)
  */
-int sipstack_send_invite(int callId, char *to, char *from, char *subject);
+int sipstack_send_invite(char *to, char *from, char *subject);
 
 /**
  * Send an reINVITE. Therefore a call id is needed
  * to identify the call this reINVITE belongs to.
  *
- * @param callId call id
+ * @param dialogId dialog id
  * @return return code (-1 if sending of reINVITE failed)
  */
-int sipstack_send_reinvite(int callId);
+int sipstack_send_reinvite(int dialogId);
 
 /**
  * Terminate a call by sending BYE, CANCEL or DECLINE.
@@ -128,9 +127,10 @@ int sipstack_send_reinvite(int callId);
  * done by this method.
  *
  * @param callId call id
+ * @param dialogId dialog id
  * @return method result code
  */
-int sipstack_terminate(int callId);
+int sipstack_terminate(int callId, int dialogId) {
 
 /**
  * Terminate a call by sending BYE.
@@ -138,9 +138,10 @@ int sipstack_terminate(int callId);
  * done by this method.
  *
  * @param callId call id
+ * @param dialogId dialog id
  * @return method result code
  */
-int sipstack_bye(int callId);
+int sipstack_bye(int callId, int dialogId);
 
 /**
  * Terminate a call by sending CANCEL.
@@ -148,9 +149,10 @@ int sipstack_bye(int callId);
  * done by this method.
  *
  * @param callId call id
+ * @param dialogId dialog id
  * @return method result code
  */
-int sipstack_cancel(int callId);
+int sipstack_cancel(int callId, int dialogId);
 
 /**
  * Terminate a call by sending DECLINE.
@@ -158,36 +160,38 @@ int sipstack_cancel(int callId);
  * done by this method.
  *
  * @param callId call id
+ * @param dialogId dialog id
  * @return method result code
  */
-int sipstack_decline(int callId);
+int sipstack_decline(int callId, int dialogId);
 
 /**
  * Send a 200 OK.
  * This method sends a 200 OK as a response to an INVITE or BYE.
  *
- * @param callId call id
+ * @param dialogId dialog id
+ * @param transactionId transaction id
  * @return method result code
  */
-int sipstack_send_ok(int callId);
+int sipstack_send_ok(int dialogId, int transactionId);
 
 /**
  * Build and send a default ACK for a 200 OK received.
  * This method sends a 200 OK as a response to an INVITE or BYE.
  *
- * @param callId call id
+ * @param dialogId call id
  * @return method result code
  */
-int sipstack_send_acknowledgment(int callId);
+int sipstack_send_acknowledgment(int dialogId);
 
 /**
  * Build and send a status code message.
  *
- * @param callId call id
+ * @param transactionId call id
  * @param status_code status code to send in message
  * @return method result code
  */
-int sipstack_send_status_code(int callId, int status_code);
+int sipstack_send_status_code(int transactionId, int status_code);
 
 /**
  * Set sip stack listener.
