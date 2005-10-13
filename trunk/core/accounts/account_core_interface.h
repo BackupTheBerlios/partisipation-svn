8126cb62d74cafcd1bd60a6b28864d5c2557868c
anton <anton@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-10-13 07:52:43 +0000

    - declaration of am_get_all_accounts and am_get_account

--- trunk/core/accounts/account_core_interface.h
+++ trunk/core/accounts/account_core_interface.h
@@ -1,8 +1,14 @@
 #ifndef HACCOUNT_CORE_INTERFACE_USED
 #define HACCOUNT_CORE_INTERFACE_USED
 
+#include "list.h"
+
 void am_set_xml_source(char *file);
 
 void am_init();
+
+void am_get_all_accounts(struct account *accounts[], int *length);
+
+struct account *am_get_account(int const accountId);
 
 #endif
