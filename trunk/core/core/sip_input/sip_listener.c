0fdd237e9928f0c01903f64bee7644670ec731ba
mliebig <mliebig@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-10-27 17:53:27 +0000

    core: - added sip listener on core side (sip event listener) which transforms   sip events to statemachine events - added dummy sip output interface to improve component design

---
+++ trunk/core/core/sip_input/sip_listener.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <util/logging/logger.h>
+#include <sipstack/sip_listener_interface.h>
+#include <core/events/events.h>
+#include <core/events/event_dispatcher.h>
+
+#define SIP_LISTENER_MSG_PREFIX "[sip listener]"
+
+void sip_listener_receive_event(sipstack_event * event) {
+	int rc;
+	void **params;
+	params = (void **) malloc(sizeof(sipstack_event *));
+	params[0] = (void *) event;
+	rc = event_dispatch(SIPLISTENER_RECEIVE, params);
+	if (!rc) {
+		// ERROR
+	}
+}
