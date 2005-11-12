694d3110bc2ff5ed7d35a73e3476e5525ac138ff
mliebig <mliebig@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-11-12 20:13:36 +0000

    GUI callback (remote): - now with real error handling (Core is not stopped because of a XML-RPC   error) - GUI URL is checked before it is used - improved/corrected logging

--- trunk/core/remote/callback/xmlrpc/gui_callback.c
+++ trunk/core/remote/callback/xmlrpc/gui_callback.c
@@ -9,116 +9,142 @@
 #include <util/config/globals.h>
 #include <util/logging/logger.h>
 
-#define GUI_CALLBACK_MSG_PREFIX "[gui callback]"
+#define GUI_CALLBACK_MSG_PREFIX "[gui callback] "
 
-void die_if_fault_occurred(xmlrpc_env * env) {
+int xmlrpc_error_occurred(xmlrpc_env * env, const char *methodName) {
 	if (env->fault_occurred) {
-		log_message(LOG_DEBUG,
-					GUI_CALLBACK_MSG_PREFIX
-					"XML-RPC Fault: %s (%d)",
-					env->fault_string, env->fault_code);
-		exit(1);
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX "XML-RPC Fault occurred while "
+				  "trying to call %s method at <%s>: %s (%d)", methodName,
+				  config.remote.callback.guiCallback.guiURL,
+				  env->fault_string, env->fault_code);
+
+		// clear error from xmlrpc environment:
+		xmlrpc_env_clean(env);
+		xmlrpc_env_init(env);
+
+		// current URL is no longer valid, prevent other functions from 
+		// calling it:
+		free(config.remote.callback.guiCallback.guiURL);
+		config.remote.callback.guiCallback.guiURL = NULL;
+		return 1;
 	}
+	return 0;
 }
 
 int change_reg_status(int accountId, int registered) {
-
 	xmlrpc_value *result;
 	xmlrpc_bool success;
 	const char *methodName = "gui.changeRegStatus";
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering changeRegStatus...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.changeRegStatus...");
+
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX "GUI URL is not set, can not "
+				  "alter registration status");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "(ib)", (xmlrpc_int32) accountId,
 						   (xmlrpc_bool) registered);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.changeRegStatus returned "
+			  "with %d", success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "leaving changeRegStatus");
-
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.changeRegStatus");
 	return success;
 }
 
 int change_call_status(int callId, char *status) {
-
 	xmlrpc_value *result;
 	xmlrpc_bool success;
 	const char *methodName = "gui.changeCallStatus";
 
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.changeCallStatus...");
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering changeCallStatus...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX
+				  "GUI URL is not set, can not " "alter call status");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "(is)", (xmlrpc_int32) callId,
 						   status);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.changeCallStatus returned "
+			  "with %d", success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "leaving changeCallStatus...");
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.changeCallStatus...");
 	return success;
 }
 
-int show_user_event(int accountId,
-				char *category,
-				char *title, char *message, char *detailMessage) {
+int show_user_event(int accountId, char *category, char *title,
+					char *message, char *detailMessage) {
 
 	xmlrpc_value *result;
 	xmlrpc_bool success;
 	const char *methodName = "gui.showUserEvent";
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering showUserEvent...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.showUserEvent...");
+
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX
+				  "GUI URL is not set, can not " "show user event");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
-						   config.remote.callback.guiCallback.guiURL,
-						   methodName, "(issss)", (xmlrpc_int32) accountId,
-						   category, title, message, detailMessage);
-	die_if_fault_occurred(&callbackEnv);
+						   config.remote.callback.guiCallback.
+						   guiURL, methodName, "(issss)",
+						   (xmlrpc_int32) accountId, category, title,
+						   message, detailMessage);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.showUserEvent returned with %d",
+			  success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "leaving showUserEvent");
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.showUserEvent");
 	return success;
 }
 
@@ -127,63 +153,76 @@ int register_core() {
 	xmlrpc_bool success;
 	const char *methodName = "gui.registerCore";
 
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.registerCore...");
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering registerCore...");
-
-	/*
-	 * Make the remote procedure call 
-	 */
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX
+				  "GUI URL is not set, can not " "register Core");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "()", NULL);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d\n", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.registerCore returned with %d",
+			  success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
-	log_message(LOG_DEBUG, GUI_CALLBACK_MSG_PREFIX "leaving registerCore");
 
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.registerCore");
 	return success;
 }
 
-int
-incoming_call(int accountId,
-			  int callId, char *callerSipUri, char *callerDisplayName) {
+int incoming_call(int accountId, int callId, char *callerSipUri,
+				  char *callerDisplayName) {
+
 	xmlrpc_value *result;
 	xmlrpc_bool success;
 	const char *methodName = "gui.incomingCall";
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering incomingCall...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.incomingCall...");
+
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX
+				  "GUI URL is not set, can not " "signal incoming call");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "(iiss)", (xmlrpc_int32) accountId,
 						   (xmlrpc_int32) callId, callerSipUri,
 						   callerDisplayName);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.incomingCall returned with %d",
+			  success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-	log_message(LOG_DEBUG, GUI_CALLBACK_MSG_PREFIX "leaving incomingCall");
-	
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.incomingCall");
 	return success;
 }
 
@@ -192,29 +231,35 @@ int set_speaker_volume_cb(double level) {
 	xmlrpc_bool success;
 	const char *methodName = "gui.setSpeakerVolume";
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering setSpeakerVolume...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.setSpeakerVolume...");
+
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX "GUI URL is not set, can not "
+				  "set speaker volume");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "(d)", (xmlrpc_double) level);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.setSpeakerVolume returned "
+			  "with %d", success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "leaving setSpeakerVolume");
-
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.setSpeakerVolume");
 	return success;
 }
 
@@ -223,28 +268,34 @@ int set_micro_volume_cb(double level) {
 	xmlrpc_bool success;
 	const char *methodName = "gui.setMicroVolume";
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "entering setMicroVolume...");
-	/*
-	 * Make the remote procedure call 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "entering GUI.setMicroVolume...");
+
+	if (!config.remote.callback.guiCallback.guiURL) {
+		LOG_ERROR(GUI_CALLBACK_MSG_PREFIX "GUI URL is not set, can not "
+				  "set microphone volume");
+		return 0;
+	}
+	// make the remote procedure call
 	result =
 		xmlrpc_client_call(&callbackEnv,
 						   config.remote.callback.guiCallback.guiURL,
 						   methodName, "(d)", (xmlrpc_double) level);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
 	xmlrpc_read_bool(&callbackEnv, result, &success);
-	die_if_fault_occurred(&callbackEnv);
-	log_message(LOG_INFO, GUI_CALLBACK_MSG_PREFIX "return %d", success);
+	if (xmlrpc_error_occurred(&callbackEnv, methodName)) {
+		return 0;
+	}
 
-	/*
-	 * Dispose of our result value. 
-	 */
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "GUI.setMicroVolume returned "
+			  "with %d", success);
+
+	// dispose result value
 	xmlrpc_DECREF(result);
 
-	log_message(LOG_DEBUG,
-				GUI_CALLBACK_MSG_PREFIX "leaving setMicroVolume");
-
+	LOG_DEBUG(GUI_CALLBACK_MSG_PREFIX "leaving GUI.setMicroVolume");
 	return success;
 }
