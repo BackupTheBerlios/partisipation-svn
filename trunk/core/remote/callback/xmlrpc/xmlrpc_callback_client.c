881d3f1d897c2a1e5fa98c4be6143e248ddd951e
mliebig <mliebig@c0e16569-72f7-0310-b75c-ade257c83c2b>
2005-11-12 20:05:36 +0000

    remote/callback: - improved error handling in callback client

--- trunk/core/remote/callback/xmlrpc/xmlrpc_callback_client.c
+++ trunk/core/remote/callback/xmlrpc/xmlrpc_callback_client.c
@@ -7,35 +7,37 @@
 #include <remote/callback/xmlrpc/xmlrpc_callback_client.h>
 #include <util/config/globals.h>
 
+#define XMLRPC_CALLBACK_CLIENT_PREFIX "[xmlrpc callback client] "
+
 xmlrpc_env callbackEnv;
 
 int cb_init() {
-	/*
-	 * Initialize our error-handling environment. 
-	 */
+	// initialize error-handling environment
 	xmlrpc_env_init(&callbackEnv);
 
-	/*
-	 * Start up our XML-RPC client library. 
-	 */
+	// start up XML-RPC client library
 	xmlrpc_client_init2(&callbackEnv, XMLRPC_CLIENT_NO_FLAGS,
 						config.remote.callback.xmlrpcClient.name,
 						config.remote.callback.xmlrpcClient.version, NULL,
 						0);
-	die_if_fault_occurred(&callbackEnv);
+
+	if (callbackEnv.fault_occurred) {
+		LOG_ERROR(XMLRPC_CALLBACK_CLIENT_PREFIX "XML-RPC Fault occurred "
+				  "while trying to initialize XML-RPC client library: "
+				  "%s (%d)", callbackEnv.fault_string,
+				  callbackEnv.fault_code);
+		xmlrpc_env_clean(&callbackEnv);
+		return 0;
+	}
 
 	return 1;
 }
 
 int cb_destroy() {
-	/*
-	 * Clean up our error-handling environment. 
-	 */
+	// clean up error-handling environment
 	xmlrpc_env_clean(&callbackEnv);
 
-	/*
-	 * Shutdown our XML-RPC client library. 
-	 */
+	// shutdown XML-RPC client library
 	xmlrpc_client_cleanup();
 
 	return 1;
